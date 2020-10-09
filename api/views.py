6243f0c56f8af52f80f46639a343e52815b43aba
GoldenMao <zhangli568101@163.com>
2020-10-09 20:43:09 +0800

    test commit

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
+# 所有视图函数在此书写，包括前后端数据交互函数
+
 import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # 返回首页界面
 def index(request):
     return render(request, "index.html")
+
 
 # 测试函数，用于测试前后端的数据连通
 def api_data_test(request):
